4fcf0146d7801984da4ae54327fb5af07d544324
Daniel Winkelman <dwinkelman3@gmail.com>
2022-04-29 15:39:27 -0400

    [lookup] T-to-int lookup table implementation

---
+++ tests/LookupTests.cc
@@ -0,0 +1,15 @@
+// Copyright 2022 by Daniel Winkelman. All rights reserved.
+
+#include <Lookup.h>
+#include <gtest/gtest.h>
+
+namespace algo {
+TEST(Lookup, Subscript) {
+  Lookup<std::string> lookup;
+  ASSERT_EQ(lookup("hello"), 0);
+  ASSERT_EQ(lookup("world"), 1);
+  ASSERT_EQ(lookup(static_cast<uint32_t>(0)), "hello");
+  ASSERT_EQ(lookup(1), "world");
+  ASSERT_THROW(lookup(2), std::out_of_range);
+}
+}  // namespace algo

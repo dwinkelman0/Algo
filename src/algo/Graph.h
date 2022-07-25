4779dff13882beab7801205153a730a34077f1b3
Daniel Winkelman <dwinkelman3@gmail.com>
2022-07-25 16:39:00 -0400

    [graph] Basic graph algorithms (i.e. DFS)

--- src/algo/Graph.h
+++ src/algo/Graph.h
@@ -7,12 +7,13 @@
 #include <map>
 #include <memory>
 #include <set>
+#include <stack>
 #include <variant>
 
 namespace algo {
 class Unit {};
 
-template <typename D, typename E, typename V>
+template <typename D, typename E, typename V, bool IsDirected>
 class Graph {
  protected:
   template <typename I, typename R>
@@ -49,6 +50,13 @@ class Graph {
     Range(const I& begin, const I& end) : begin_(begin), end_(end) {}
     I begin() const { return begin_; }
     I end() const { return end_; }
+    uint32_t getCount() const {
+      uint32_t count = 0;
+      for (auto it = begin(); it != end(); ++it) {
+        ++count;
+      }
+      return count;
+    }
 
    private:
     I begin_, end_;
@@ -84,6 +92,20 @@ class Graph {
 
   using VertexIterator = Iterator<VertexMapIterator, Vertex>;
   using VertexConstIterator = Iterator<VertexMapConstIterator, ConstVertex>;
+
+  using VertexVariant =
+      std::variant<uint32_t, Vertex, VertexIterator, VertexConstIterator>;
+  class VertexVariantVisitor {
+   public:
+    uint32_t operator()(const uint32_t index) { return index; }
+    uint32_t operator()(const Vertex& vertex) { return vertex.getIndex(); }
+    uint32_t operator()(const VertexIterator& vertexIt) {
+      return vertexIt->getIndex();
+    }
+    uint32_t operator()(const VertexConstIterator& vertexIt) {
+      return vertexIt->getIndex();
+    }
+  };
 
  protected:
   class EdgeMapComparator;
@@ -134,7 +156,8 @@ class Graph {
       if (isOutbound_) {
         return a.first < b.first || (a.first == b.first && a.second < b.second);
       } else {
-        return b.first < a.first || (b.first == a.first && b.second < a.second);
+        return a.second < b.second ||
+               (a.second == b.second && a.first < b.first);
       }
     }
 
@@ -148,15 +171,31 @@ class Graph {
 
  public:
   Graph()
-      : outboundEdges_(typename Graph<D, E, V>::EdgeMapComparator(true)),
-        inboundEdges_(typename Graph<D, E, V>::EdgeMapComparator(false)) {}
+      : outboundEdges_(EdgeMapComparator(true)),
+        inboundEdges_(EdgeMapComparator(false)) {}
 
   inline std::pair<VertexIterator, bool> emplaceVertex(const uint32_t a) {
     return vertices_.emplace(a, V());
   }
 
-  virtual std::pair<EdgeIterator, bool> emplaceEdge(const uint32_t a,
-                                                    const uint32_t b) = 0;
+  std::pair<EdgeIterator, bool> emplaceEdge(const uint32_t a,
+                                            const uint32_t b) {
+    auto it = this->outboundEdges_.find(std::pair<uint32_t, uint32_t>(a, b));
+    if (it != this->outboundEdges_.end()) {
+      return std::pair<EdgeIterator, bool>(it, false);
+    } else {
+      std::pair<uint32_t, uint32_t> forward(a, b);
+      auto mem = std::make_shared<std::pair<D, E>>(D(), E());
+      it = this->outboundEdges_.emplace(forward, mem).first;
+      this->inboundEdges_.emplace(forward, mem);
+      if (!IsDirected) {
+        std::pair<uint32_t, uint32_t> reverse(b, a);
+        this->inboundEdges_.emplace(reverse, mem);
+        this->outboundEdges_.emplace(reverse, mem);
+      }
+      return std::pair<EdgeIterator, bool>(it, true);
+    }
+  }
 
   Range<VertexIterator> getVertices() {
     return Range<VertexIterator>(vertices_.begin(), vertices_.end());
@@ -166,18 +205,141 @@ class Graph {
     return Range<VertexConstIterator>(vertices_.begin(), vertices_.end());
   }
 
-  virtual Range<EdgeIterator> getEdges() = 0;
-  virtual Range<EdgeConstIterator> getEdges() const = 0;
-
-  Range<EdgeIterator> getEdgesFromVertex(const uint32_t a) {
-    return Range<EdgeIterator>(
-        outboundEdges_.lower_bound(std::pair<uint32_t, uint32_t>(a, 0)),
-        outboundEdges_.upper_bound(std::pair<uint32_t, uint32_t>(a + 1, 0)));
+  std::optional<VertexIterator> getVertex(const VertexVariant& vertex) {
+    auto it = vertices_.find(std::visit(VertexVariantVisitor(), vertex));
+    return it == vertices_.end() ? std::nullopt
+                                 : std::optional<VertexIterator>(it);
   }
 
-  Range<EdgeConstIterator> getEdgesFromVertex(const uint32_t a) const;
-  Range<EdgeIterator> getEdgesToVertex(const uint32_t a);
-  Range<EdgeConstIterator> getEdgesToVertex(const uint32_t a) const;
+  std::optional<VertexConstIterator> getVertex(
+      const VertexVariant& vertex) const {
+    auto it = vertices_.find(std::visit(VertexVariantVisitor(), vertex));
+    return it == vertices_.end() ? std::nullopt
+                                 : std::optional<VertexConstIterator>(it);
+  }
+
+  Range<EdgeIterator> getEdges() {
+    std::function skipper = [this](const EdgeIterator& it) {
+      return it != this->outboundEdges_.end() &&
+             it->getSource() > it->getDest();
+    };
+    return Range<EdgeIterator>(
+        IsDirected ? EdgeIterator(this->outboundEdges_.begin())
+                   : EdgeIterator(this->outboundEdges_.begin(), skipper),
+        EdgeIterator(this->outboundEdges_.end()));
+  }
+
+  Range<EdgeConstIterator> getEdges() const {
+    std::function skipper = [this](const EdgeConstIterator& it) {
+      return it != this->outboundEdges_.end() &&
+             it->getSource() > it->getDest();
+    };
+    return Range<EdgeConstIterator>(
+        IsDirected ? EdgeConstIterator(this->outboundEdges_.begin())
+                   : EdgeConstIterator(this->outboundEdges_.begin(), skipper),
+        EdgeConstIterator(this->outboundEdges_.end()));
+  }
+
+  Range<EdgeIterator> getEdgesFromVertex(const VertexVariant& vertex) {
+    uint32_t index = std::visit(VertexVariantVisitor(), vertex);
+    return Range<EdgeIterator>(
+        outboundEdges_.lower_bound(std::pair<uint32_t, uint32_t>(index, 0)),
+        outboundEdges_.upper_bound(std::pair<uint32_t, uint32_t>(
+            index, std::numeric_limits<uint32_t>().max())));
+  }
+
+  Range<EdgeConstIterator> getEdgesFromVertex(
+      const VertexVariant& vertex) const {
+    uint32_t index = std::visit(VertexVariantVisitor(), vertex);
+    return Range<EdgeConstIterator>(
+        outboundEdges_.lower_bound(std::pair<uint32_t, uint32_t>(index, 0)),
+        outboundEdges_.upper_bound(std::pair<uint32_t, uint32_t>(
+            index, std::numeric_limits<uint32_t>().max())));
+  }
+
+  Range<EdgeIterator> getEdgesToVertex(const VertexVariant& vertex) {
+    uint32_t index = std::visit(VertexVariantVisitor(), vertex);
+    return Range<EdgeIterator>(
+        inboundEdges_.lower_bound(std::pair<uint32_t, uint32_t>(0, index)),
+        inboundEdges_.upper_bound(std::pair<uint32_t, uint32_t>(
+            std::numeric_limits<uint32_t>().max(), index)));
+  }
+
+  Range<EdgeConstIterator> getEdgesToVertex(const VertexVariant& vertex) const {
+    uint32_t index = std::visit(VertexVariantVisitor(), vertex);
+    return Range<EdgeConstIterator>(
+        inboundEdges_.lower_bound(std::pair<uint32_t, uint32_t>(0, index)),
+        inboundEdges_.upper_bound(std::pair<uint32_t, uint32_t>(
+            std::numeric_limits<uint32_t>().max(), index)));
+  }
+
+ protected:
+  using VertexFunction = std::function<void(const ConstVertex&)>;
+  using EdgeFunction = std::function<void(const ConstEdge&)>;
+
+  template <bool Reverse>
+  void traverseDepthFirst(const VertexFunction& vertexFunc,
+                          const EdgeFunction& edgeFunc, const uint32_t index,
+                          std::set<uint32_t>& visited) const {
+    if (visited.find(index) == visited.end()) {
+      visited.insert(index);
+      std::optional<VertexConstIterator> vertexIt = getVertex(index);
+      if (!vertexIt) {
+        throw std::exception();
+      }
+      vertexFunc(**vertexIt);
+      for (const ConstEdge& edge : getEdgesFromVertex(index)) {
+        uint32_t dest = edge.getDest();
+        if (IsDirected || dest >= index) {
+          edgeFunc(edge);
+        }
+        traverseDepthFirst<Reverse>(vertexFunc, edgeFunc, dest, visited);
+      }
+      if (Reverse) {
+        for (const ConstEdge& edge : getEdgesToVertex(index)) {
+          uint32_t source = edge.getSource();
+          if (IsDirected || source <= index) {
+            edgeFunc(edge);
+          }
+          traverseDepthFirst<Reverse>(vertexFunc, edgeFunc, source, visited);
+        }
+      }
+    }
+  }
+
+ public:
+  std::set<uint32_t> traverseDepthFirst(const VertexFunction& vertexFunc,
+                                        const EdgeFunction& edgeFunc,
+                                        const VertexVariant firstVertex) const {
+    std::set<uint32_t> vertices;
+    traverseDepthFirst<false>(vertexFunc, edgeFunc,
+                              std::visit(VertexVariantVisitor(), firstVertex),
+                              vertices);
+    return vertices;
+  }
+
+  std::vector<Graph> getConnectedComponents() const {
+    std::vector<Graph> output;
+    std::set<uint32_t> vertices;
+    while (vertices.size() < vertices_.size()) {
+      for (const ConstVertex& vertex : getVertices()) {
+        if (vertices.find(vertex.getIndex()) == vertices.end()) {
+          Graph subgraph;
+          traverseDepthFirst<true>(
+              [&subgraph](const ConstVertex& vertex) {
+                subgraph.emplaceVertex(vertex.getIndex());
+              },
+              [&subgraph](const ConstEdge& edge) {
+                subgraph.emplaceVertex(edge.getDest());
+                subgraph.emplaceEdge(edge.getSource(), edge.getDest());
+              },
+              vertex.getIndex(), vertices);
+          output.push_back(subgraph);
+        }
+      }
+    }
+    return output;
+  }
 
  protected:
   VertexMap vertices_;
@@ -185,90 +347,8 @@ class Graph {
 };
 
 template <typename D, typename E, typename V>
-class UndirectedGraph : public Graph<D, E, V> {
- public:
-  template <typename I>
-  using Range = typename Graph<D, E, V>::template Range<I>;
-  using EdgeIterator = typename Graph<D, E, V>::EdgeIterator;
-  using EdgeConstIterator = typename Graph<D, E, V>::EdgeConstIterator;
-
- public:
-  UndirectedGraph() : Graph<D, E, V>() {}
-
-  std::pair<EdgeIterator, bool> emplaceEdge(const uint32_t a,
-                                            const uint32_t b) override {
-    auto it = this->outboundEdges_.find(std::pair<uint32_t, uint32_t>(a, b));
-    if (it != this->outboundEdges_.end()) {
-      return std::pair<EdgeIterator, bool>(it, false);
-    } else {
-      std::pair<uint32_t, uint32_t> forward(a, b);
-      std::pair<uint32_t, uint32_t> reverse(b, a);
-      auto mem = std::make_shared<std::pair<D, E>>(D(), E());
-      it = this->outboundEdges_.emplace(forward, mem).first;
-      this->outboundEdges_.emplace(reverse, mem);
-      this->inboundEdges_.emplace(forward, mem);
-      this->inboundEdges_.emplace(reverse, mem);
-      return std::pair<EdgeIterator, bool>(it, true);
-    }
-  }
-
-  Range<EdgeIterator> getEdges() override {
-    std::function skipper =
-        [this](const typename Graph<D, E, V>::EdgeIterator& it) {
-          return it != this->outboundEdges_.end() &&
-                 it->getSource() > it->getDest();
-        };
-    return Range<EdgeIterator>(
-        EdgeIterator(this->outboundEdges_.begin(), skipper),
-        EdgeIterator(this->outboundEdges_.end()));
-  }
-
-  Range<EdgeConstIterator> getEdges() const override {
-    std::function skipper =
-        [this](const typename Graph<D, E, V>::EdgeConstIterator& it) {
-          return it != this->outboundEdges_.end() &&
-                 it->getSource() > it->getDest();
-        };
-    return Range<EdgeConstIterator>(
-        EdgeConstIterator(this->outboundEdges_.begin(), skipper),
-        EdgeConstIterator(this->outboundEdges_.end()));
-  }
-};
+using UndirectedGraph = Graph<D, E, V, false>;
 
 template <typename D, typename E, typename V>
-class DirectedGraph : public Graph<D, E, V> {
- public:
-  template <typename I>
-  using Range = typename Graph<D, E, V>::template Range<I>;
-  using EdgeIterator = typename Graph<D, E, V>::EdgeIterator;
-  using EdgeConstIterator = typename Graph<D, E, V>::EdgeConstIterator;
-
- public:
-  DirectedGraph() : Graph<D, E, V>() {}
-
-  std::pair<EdgeIterator, bool> emplaceEdge(const uint32_t a,
-                                            const uint32_t b) override {
-    auto it = this->outboundEdges_.find(std::pair<uint32_t, uint32_t>(a, b));
-    if (it != this->outboundEdges_.end()) {
-      return std::pair<EdgeIterator, bool>(it, false);
-    } else {
-      std::pair<uint32_t, uint32_t> forward(a, b);
-      auto mem = std::make_shared<std::pair<D, E>>(D(), E());
-      it = this->outboundEdges_.emplace(forward, mem).first;
-      this->inboundEdges_.emplace(forward, mem);
-      return std::pair<EdgeIterator, bool>(it, true);
-    }
-  }
-
-  Range<EdgeIterator> getEdges() override {
-    return Range<EdgeIterator>(EdgeIterator(this->outboundEdges_.begin()),
-                               EdgeIterator(this->outboundEdges_.end()));
-  }
-
-  Range<EdgeConstIterator> getEdges() const override {
-    return Range<EdgeConstIterator>(
-        EdgeConstIterator(this->outboundEdges_.begin()),
-        EdgeConstIterator(this->outboundEdges_.end()));
-  }
-};
+using DirectedGraph = Graph<D, E, V, true>;
 }  // namespace algo
--- tests/GraphTests.cc
+++ tests/GraphTests.cc
@@ -15,19 +15,105 @@ TEST(Graph, Basic) {
   GraphType::VertexIterator v2 = graph.emplaceVertex(2).first;
   GraphType::EdgeIterator e0 = graph.emplaceEdge(0, 1).first;
   GraphType::EdgeIterator e1 = graph.emplaceEdge(1, 2).first;
-  // GraphType::EdgeIterator e2 = graph.emplaceEdge(1, 0).first;
+  GraphType::EdgeIterator e2 = graph.emplaceEdge(1, 0).first;
   v0->getValue() = {4, 5, 6};
   v1->getValue() = {1, 2, 3, 7};
   e0->getWeight() = 0.5;
   e0->getValue() = "hello";
   e1->getValue() = "world";
-  for (GraphType::Vertex vertex : graph.getVertices()) {
-    std::cout << vertex.getIndex() << ": " << vertex.getValue().size()
-              << std::endl;
+}
+
+TEST(Graph, Vertices) {
+  using GraphType = UndirectedGraph<Unit, Unit, uint32_t>;
+  GraphType graph;
+  const auto &[v0, s0] = graph.emplaceVertex(0);
+  const auto &[v1, s1] = graph.emplaceVertex(1);
+  const auto &[v2, s2] = graph.emplaceVertex(0);
+  ASSERT_EQ(v0->getIndex(), 0);
+  ASSERT_TRUE(s0);
+  ASSERT_EQ(v1->getIndex(), 1);
+  ASSERT_TRUE(s1);
+  ASSERT_EQ(v2, v0);
+  ASSERT_FALSE(s2);
+  for (const GraphType::Vertex vertex : graph.getVertices()) {
+    ASSERT_EQ(vertex.getValue(), 0);
   }
-  for (GraphType::Edge edge : graph.getEdgesFromVertex(1)) {
-    std::cout << edge.getSource() << " -> " << edge.getDest() << ": "
-              << edge.getValue() << std::endl;
-  }
+  std::optional<GraphType::VertexIterator> v3 = graph.getVertex(0u);
+  (*v3)->getValue() = 5;
+  ASSERT_EQ((*v3)->getValue(), 5);
+}
+
+TEST(Graph, DirectedDFS) {
+  using GraphType = DirectedGraph<Unit, Unit, Unit>;
+  GraphType graph;
+  graph.emplaceVertex(0);
+  graph.emplaceVertex(1);
+  graph.emplaceVertex(2);
+  graph.emplaceVertex(3);
+  graph.emplaceVertex(4);
+  graph.emplaceEdge(0, 1);
+  graph.emplaceEdge(1, 2);
+  graph.emplaceEdge(2, 0);
+  graph.emplaceEdge(0, 2);
+  graph.emplaceEdge(2, 2);
+  graph.emplaceEdge(0, 3);
+  graph.emplaceEdge(0, 4);
+  graph.emplaceEdge(4, 0);
+  graph.emplaceEdge(3, 2);
+  graph.emplaceEdge(4, 4);
+  uint32_t vertexCount = 0;
+  uint32_t edgeCount = 0;
+  graph.traverseDepthFirst(
+      [&vertexCount](const GraphType::ConstVertex &vertex) { ++vertexCount; },
+      [&edgeCount](const GraphType::ConstEdge &edge) { ++edgeCount; }, 2u);
+  ASSERT_EQ(vertexCount, 5);
+  ASSERT_EQ(edgeCount, 10);
+}
+
+TEST(Graph, UndirectedDFS) {
+  using GraphType = UndirectedGraph<Unit, Unit, Unit>;
+  GraphType graph;
+  graph.emplaceVertex(0);
+  graph.emplaceVertex(1);
+  graph.emplaceVertex(2);
+  graph.emplaceVertex(3);
+  graph.emplaceVertex(4);
+  graph.emplaceEdge(0, 1);
+  graph.emplaceEdge(1, 2);
+  graph.emplaceEdge(2, 0);
+  graph.emplaceEdge(2, 2);
+  graph.emplaceEdge(0, 3);
+  graph.emplaceEdge(0, 4);
+  graph.emplaceEdge(3, 2);
+  graph.emplaceEdge(4, 4);
+  uint32_t vertexCount = 0;
+  uint32_t edgeCount = 0;
+  graph.traverseDepthFirst(
+      [&vertexCount](const GraphType::ConstVertex &vertex) { ++vertexCount; },
+      [&edgeCount](const GraphType::ConstEdge &edge) { ++edgeCount; }, 2u);
+  ASSERT_EQ(vertexCount, 5);
+  ASSERT_EQ(edgeCount, 8);
+}
+
+TEST(Graph, DirectedConnectedComponents) {
+  using GraphType = DirectedGraph<Unit, Unit, Unit>;
+  GraphType graph;
+  graph.emplaceVertex(0);
+  graph.emplaceVertex(1);
+  graph.emplaceVertex(2);
+  graph.emplaceVertex(3);
+  graph.emplaceVertex(4);
+  graph.emplaceEdge(0, 0);
+  graph.emplaceEdge(0, 1);
+  graph.emplaceEdge(1, 2);
+  graph.emplaceEdge(3, 4);
+  graph.emplaceEdge(4, 3);
+  graph.emplaceEdge(4, 4);
+  std::vector<GraphType> components = graph.getConnectedComponents();
+  ASSERT_EQ(components.size(), 2);
+  ASSERT_EQ(components[0].getVertices().getCount(), 3);
+  ASSERT_EQ(components[0].getEdges().getCount(), 3);
+  ASSERT_EQ(components[1].getVertices().getCount(), 2);
+  ASSERT_EQ(components[1].getEdges().getCount(), 3);
 }
 }  // namespace algo
